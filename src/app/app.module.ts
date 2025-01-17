68c4ef10afb7df79271331c8bedb34151b65c89e
RafaGiaretta <Rafaelgiaretta@ymail.com>
2025-01-17 14:52:33 -0300

    Emitindo eventos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { ParentDataComponent } from './components/parent-data/parent-data.compon
 import { DiretivesComponent } from './components/diretives/diretives.component';
 import { IfRenderComponent } from './components/if-render/if-render.component';
 import { EventosComponent } from './components/eventos/eventos.component';
+import { EmitterComponent } from './components/emitter/emitter.component';
+import { ChangeNumberComponent } from './components/change-number/change-number.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { EventosComponent } from './components/eventos/eventos.component';
     ParentDataComponent,
     DiretivesComponent,
     IfRenderComponent,
-    EventosComponent
+    EventosComponent,
+    EmitterComponent,
+    ChangeNumberComponent
   ],
   imports: [
     BrowserModule,
