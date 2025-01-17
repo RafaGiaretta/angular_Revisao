e7317132de0bf1b3ba8855803559d5e57e3f406b
RafaGiaretta <Rafaelgiaretta@ymail.com>
2025-01-17 16:57:53 -0300

    Feat routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { ListRenderComponent } from './components/list-render/list-render.component';
+import { DiretivesComponent } from './components/diretives/diretives.component';
+import { EmitterComponent } from './components/emitter/emitter.component';
+import { EventosComponent } from './components/eventos/eventos.component';
+import { PipesComponent } from './components/pipes/pipes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: FirstComponentComponent },
+  { path: 'list', component: ListRenderComponent },
+  { path: 'diretives', component: DiretivesComponent },
+  { path: 'emitter', component: EmitterComponent },
+  { path: 'events', component: EventosComponent },
+  { path: 'first-comp', component: FirstComponentComponent },
+  { path: 'pipes', component: PipesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
